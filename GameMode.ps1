0bb2c3bafb055108ba9c0691e234b8f59384627d
Azarchaniel <csonkalubos@gmail.com>
2020-04-15 14:01:50 +0200

    Add files via upload
    Init

---
+++ GameMode.ps1
@@ -0,0 +1,51 @@
+﻿Import-Module "D:\Programy\AudioDeviceCmdlets.dll"   #library for controlling sound settings
+$sleepTime = 1.5
+
+try {
+    Set-AudioDevice -Index 2 > $null # >$null, so it won't write output
+    Set-AudioDevice -PlaybackVolume 18
+    Write-Host "+ Sound output changed to headphones and volume to 20"
+} catch {
+    Write-Host "--- ERROR while setting up sound" -ForegroundColor red
+    $sleepTime = 3
+}
+
+function terminateProcess {
+    param($processes)
+    try{
+        foreach($process in $processes) {
+            if (-not ( $(Get-Process -name $process -ErrorAction SilentlyContinue) -eq $null )) {
+                Stop-Process -Name $process -Force
+                Write-Host "+" $process "stopped"
+            }
+        }
+    } catch {
+        Write-Host "--- ERROR while stopping" $process -ForegroundColor red
+        $sleepTime = 3
+    }
+}
+
+$processesToStop = @("boinc", "boincmgr", "boinctray", "SkypeApp", "SkypeBackgroundHost", "FAHClient", "utweb", "YourPhone", "YourPhoneServer", "Slack", "HWiNFO64", "TaskMgr", "TeamViewer_Service")
+terminateProcess($processesToStop)
+
+try {
+    if ( $(Get-Process -name TaskMgr -ErrorAction SilentlyContinue) -eq $null ) {
+        Write-Host "+ Spusteny Task Manager"
+        C:\Windows\System32\Taskmgr.exe
+    }
+} catch {
+    Write-Host "--- TaskManager is not running" -ForegroundColor red
+    $sleepTime = 3
+}
+
+try {
+    if ( $(Get-Process -name HWiNFO64 -ErrorAction SilentlyContinue) -eq $null ) {
+        Write-Host "+ Spusteny HWiNFO"
+        C:\Program` Files\HWiNFO64\HWiNFO64.EXE
+    }
+} catch {
+    Write-Host "--- HWiNFO is not running" -ForegroundColor red
+    $sleepTime = 3
+}
+
+Start-Sleep -s $sleepTime
